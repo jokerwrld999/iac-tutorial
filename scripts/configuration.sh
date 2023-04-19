d6deabbb66fb2dfa7ccd02c093cd599865d65452
jokerwrld999 <ifalaleev49@gmail.com>
2023-04-19 06:58:41 +0000

    [scripts] Aded sudo execution

--- scripts/configuration.sh
+++ scripts/configuration.sh
@@ -2,17 +2,17 @@
 set -e
 
 echo "  ----- install ruby and bundler -----  "
-apt-get update
-apt-get install -y ruby-full build-essential
-gem install --no-rdoc --no-ri bundler
+sudo apt-get update
+sudo apt-get install -y ruby-full build-essential
+sudo gem install --no-rdoc --no-ri bundler
 
 echo "  ----- install mongodb -----  "
-apt-get install -y mongodb
+sudo apt-get install -y mongodb
 
 echo "  ----- start mongodb -----  "
-systemctl start mongod
-systemctl enable mongod
+sudo systemctl start mongod
+sudo systemctl enable mongod
 
 echo "  ----- copy unit file for application -----  "
 wget https://raw.githubusercontent.com/jokerwrld999/iac-tutorial/main/raddit-service/raddit.service
-mv raddit.service /etc/systemd/system/raddit.service
+sudo mv raddit.service /etc/systemd/system/raddit.service
