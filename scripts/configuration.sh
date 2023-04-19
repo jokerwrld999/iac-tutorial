ccf0e162bb503577c087d60025c168ef013fd44c
jokerwrld999 <ifalaleev49@gmail.com>
2023-04-19 09:15:59 +0000

    [scripts] Small fixes

--- scripts/configuration.sh
+++ scripts/configuration.sh
@@ -2,13 +2,17 @@
 set -e
 
 echo "  ----- install ruby and bundler -----  "
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt install -y ruby-dev build-essential
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y ruby-dev build-essential gnupg
 sudo gem install bundler 
 
 echo "  ----- install mongodb -----  "
-sudo apt-get install -y mongodb
+curl -fsSL https://pgp.mongodb.com/server-6.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg \
+   --dearmor
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | \
+    sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+sudo apt update && sudo apt-get install -y mongodb-org
 
 echo "  ----- start mongodb -----  "
 sudo systemctl start mongodb
