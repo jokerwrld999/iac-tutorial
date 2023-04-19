8f07fa73291ec763663d7bf42421c782725f0987
jokerwrld999 <ifalaleev49@gmail.com>
2023-04-19 07:29:14 +0000

    [script] Ruby installation fixes

--- scripts/configuration.sh
+++ scripts/configuration.sh
@@ -7,8 +7,8 @@ sudo apt-get upgrade -y
 sudo apt-get install -y build-essential
 wget https://cache.ruby-lang.org/pub/ruby/3.2/ruby-3.2.2.tar.gz
 tar xf ruby-3.2.2.tar.gz
-#cd ruby-3.2.2 && ./configure && make && sudo make install
-#sudo gem install --no-rdoc --no-ri bundler
+cd ./ruby-3.2.2 && ./configure && make && sudo make install
+sudo gem install --no-rdoc --no-ri bundler
 
 echo "  ----- install mongodb -----  "
 sudo apt-get install -y mongodb
