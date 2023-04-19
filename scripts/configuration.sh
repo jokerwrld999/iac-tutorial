bffc100bd5fb63ed4f36b76c9b93ca57f1bbc480
jokerwrld999 <ifalaleev49@gmail.com>
2023-04-19 14:06:05 +0000

    [scripts] Small fixes

--- scripts/configuration.sh
+++ scripts/configuration.sh
@@ -7,9 +7,7 @@ sudo apt install -y ruby-dev build-essential gnupg
 sudo gem install bundler 
 
 echo "  ----- install mongodb -----  "
-curl -fsSL https://pgp.mongodb.com/server-6.0.asc | \
-   sudo gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg \
-   --dearmor
+curl -fsSL https://pgp.mongodb.com/server-6.0.asc | sudo gpg --batch --yes -o /usr/share/keyrings/mongodb-server-6.0.gpg --dearmor 
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | \
     sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 sudo apt update && sudo apt-get install -y mongodb-org
