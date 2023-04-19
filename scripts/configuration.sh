f590fbe5aa20cbcc4fe4d0ee817b77dec947cf80
jokerwrld999 <ifalaleev49@gmail.com>
2023-04-19 06:51:17 +0000

    [scripts]

---
+++ scripts/configuration.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+echo "  ----- install ruby and bundler -----  "
+apt-get update
+apt-get install -y ruby-full build-essential
+gem install --no-rdoc --no-ri bundler
+
+echo "  ----- install mongodb -----  "
+apt-get install -y mongodb
+
+echo "  ----- start mongodb -----  "
+systemctl start mongod
+systemctl enable mongod
+
+echo "  ----- copy unit file for application -----  "
+wget 
+mv raddit.service /etc/systemd/system/raddit.service
