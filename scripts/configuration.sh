a6ee5ca02317e4d37846e08c6df0eaab239ed354
jokerwrld999 <ifalaleev49@gmail.com>
2023-04-19 07:27:12 +0000

    [script] Ruby installation fixes

--- scripts/configuration.sh
+++ scripts/configuration.sh
@@ -4,8 +4,11 @@ set -e
 echo "  ----- install ruby and bundler -----  "
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y ruby-full build-essential
-sudo gem install bundler -v "$(grep -A 1 "BUNDLED WITH" Gemfile.lock | tail -n 1)"
+sudo apt-get install -y build-essential
+wget https://cache.ruby-lang.org/pub/ruby/3.2/ruby-3.2.2.tar.gz
+tar xf ruby-3.2.2.tar.gz
+#cd ruby-3.2.2 && ./configure && make && sudo make install
+#sudo gem install --no-rdoc --no-ri bundler
 
 echo "  ----- install mongodb -----  "
 sudo apt-get install -y mongodb
