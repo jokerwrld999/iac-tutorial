7bf27818e5ca84907d30bfae844d3cef69149f05
jokerwrld999 <ifalaleev49@gmail.com>
2023-04-21 17:29:03 +0000

    [script] Mongodb fix

--- scripts/configuration.sh
+++ scripts/configuration.sh
@@ -2,16 +2,15 @@
 set -e
 
 echo "  ----- install ruby and bundler -----  "
-export DEBIAN_FRONTEND='noninteractive'
-sudo apt-get update
+sudo apt update
 sudo apt-get install -y ruby-dev build-essential gnupg
 sudo gem install bundler 
 
 echo "  ----- install mongodb -----  "
-curl -fsSL https://pgp.mongodb.com/server-6.0.asc | sudo gpg --batch --yes -o /usr/share/keyrings/mongodb-server-6.0.gpg --dearmor 
-echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt-get/ubuntu jammy/mongodb-org/6.0 multiverse" | \
-    sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
-sudo apt-get update && sudo apt-get install -y mongodb-org
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc |  gpg --dearmor | sudo tee /usr/share/keyrings/mongodb.gpg > /dev/null
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+sudo apt update
+sudo apt install mongodb-org
 
 echo "  ----- start mongodb -----  "
 sudo systemctl start mongod
@@ -22,4 +21,4 @@ wget https://raw.githubusercontent.com/jokerwrld999/iac-tutorial/main/raddit-ser
 sudo mv raddit.service /etc/systemd/system/raddit.service
 
 echo "  ----- deploy application -----  "
-#$HOME/iac-tutorial/scripts/deploy.sh
+#$HOME/iac-tutorial/scripts/deploy.shI
