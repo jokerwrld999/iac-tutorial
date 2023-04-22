efd075acf6d4fda055a5eb44ecf4297994ceb039
jokerwrld999 <ifalaleev49@gmail.com>
2023-04-22 15:53:30 +0300

    [script] Packages update

--- scripts/configuration.sh
+++ scripts/configuration.sh
@@ -2,12 +2,15 @@
 set -e
 
 echo "  ----- install ruby and bundler -----  "
-sudo add-apt-repository -y universe multiverse
+sudo add-apt-repository -y universe multiverse restricted
 sudo apt update
 sudo apt install -y ruby ruby-dev build-essential wget curl gnupg2 software-properties-common apt-transport-https ca-certificates lsb-release
 sudo gem install bundler 
 
 echo "  ----- install mongodb -----  "
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc |  gpg --dearmor | sudo tee /usr/share/keyrings/mongodb.gpg > /dev/null
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+sudo apt update
 sudo apt install mongodb-org
 
 echo "  ----- start mongodb -----  "
