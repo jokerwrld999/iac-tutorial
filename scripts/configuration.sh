dcba4884238359fc399b88e54bc08996f70a254c
jokerwrld999 <ifalaleev49@gmail.com>
2023-04-22 15:23:25 +0300

    [scripts] Mongodb update

--- scripts/configuration.sh
+++ scripts/configuration.sh
@@ -3,12 +3,14 @@ set -e
 
 echo "  ----- install ruby and bundler -----  "
 sudo apt update
-sudo apt install -y ruby-full build-essential wget curl gnupg2 software-properties-common apt-transport-https ca-certificates lsb-release
+sudo apt install -y ruby rubu-dev build-essential wget curl gnupg2 software-properties-common apt-transport-https ca-certificates lsb-release
 sudo gem install bundler 
 
 echo "  ----- install mongodb -----  "
 curl -fsSL https://www.mongodb.org/static/pgp/server-6.0.asc | sudo gpg --batch --yes --dearmor -o /etc/apt/trusted.gpg.d/mongodb-6.gpg
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.16_amd64.deb
+sudo dpkg -i ./libssl1.1_1.1.1f-1ubuntu2.16_amd64.deb
 sudo apt update
 sudo apt install mongodb-org
 
