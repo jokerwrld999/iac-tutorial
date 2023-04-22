3ed076884d377001caf946387a6a47900ab7965f
jokerwrld999 <ifalaleev49@gmail.com>
2023-04-22 15:55:42 +0300

    [script] Small update

--- scripts/configuration.sh
+++ scripts/configuration.sh
@@ -11,13 +11,13 @@ echo "  ----- install mongodb -----  "
 wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc |  gpg --dearmor | sudo tee /usr/share/keyrings/mongodb.gpg > /dev/null
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 sudo apt update
-sudo apt install mongodb-org
+sudo apt install -y mongodb-org
 
 echo "  ----- start mongodb -----  "
 sudo systemctl enable --now mongod
 
 echo "  ----- copy unit file for application -----  "
-sudo -O /etc/systemd/system/raddit.service wget https://raw.githubusercontent.com/jokerwrld999/iac-tutorial/main/raddit-service/raddit.service
+sudo wget -O /etc/systemd/system/raddit.service https://raw.githubusercontent.com/jokerwrld999/iac-tutorial/main/raddit-service/raddit.service
 
 echo "  ----- deploy application -----  "
 pwd
