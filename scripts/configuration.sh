3da63e232a7f86ba2170c7541abcbee78abcd3d9
jokerwrld999 <ifalaleev49@gmail.com>
2023-04-26 15:14:57 +0300

    [ansible] Deployment configured

--- scripts/configuration.sh
+++ scripts/configuration.sh
@@ -2,9 +2,9 @@
 set -e
 
 echo "  ----- install ruby and bundler -----  "
-sudo add-apt-repository -y universe 
-sudo apt update
-sudo apt install -y ruby-full build-essential gnupg
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install -y ruby-full build-essential gnupg
 sudo gem install -N bundler
 
 echo "  ----- install mongodb -----  "
@@ -12,16 +12,11 @@ curl -fsSL https://pgp.mongodb.com/server-6.0.asc | \
    sudo gpg --batch --yes -o /usr/share/keyrings/mongodb-server-6.0.gpg \
    --dearmor
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
-echo "deb http://security.ubuntu.com/ubuntu focal-security main" | sudo tee /etc/apt/sources.list.d/focal-security.list
 sudo apt-get update
-sudo apt-get install -y libssl1.1 mongodb-org
-sudo rm /etc/apt/sources.list.d/focal-security.list
+sudo apt-get install -y mongodb-org
 
 echo "  ----- start mongodb -----  "
 sudo systemctl enable --now mongod
 
 echo "  ----- copy unit file for application -----  "
 sudo wget -O /etc/systemd/system/raddit.service https://raw.githubusercontent.com/jokerwrld999/iac-tutorial/main/raddit-service/raddit.service
-
-echo "  ----- deploy application -----  "
-#$HOME/iac-tutorial/scripts/deploy.sh
